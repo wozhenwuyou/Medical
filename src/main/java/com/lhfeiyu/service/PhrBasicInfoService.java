8532b48f48e668a28aa63b596d954f4a896653c7
邹文继 <598371455@qq.com>
2017-07-03 09:20:48 +0800

    先执行数据库脚本 7.3 1增加后台查询统计功能。

--- src/main/java/com/lhfeiyu/service/PhrBasicInfoService.java
+++ src/main/java/com/lhfeiyu/service/PhrBasicInfoService.java
@@ -2,7 +2,6 @@ package com.lhfeiyu.service;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -18,6 +17,7 @@ import com.lhfeiyu.po.PhrBasicInfo;
 import com.lhfeiyu.po.PhrBasicInfoExample;
 import com.lhfeiyu.po.PhrCover;
 import com.lhfeiyu.vo.PhrBasicInfoCmd;
+import com.lhfeiyu.vo.PhrCountCmd;
 
 @Service
 public class PhrBasicInfoService {
@@ -54,29 +54,29 @@ public class PhrBasicInfoService {
 			if (StringUtils.isNotBlank(cmd.getTel())) {
 				c.andTelLike("%" + cmd.getTel() + "%");
 			}
-			if(StringUtils.isNotBlank(cmd.getJddw())){
+			if (StringUtils.isNotBlank(cmd.getJddw())) {
 				c.andJddwLike("%" + cmd.getJddw() + "%");
 			}
-			if(cmd.getCreateUserId() != null && cmd.getCreateUserId() > 0){
+			if (cmd.getCreateUserId() != null && cmd.getCreateUserId() > 0) {
 				c.andDoctorIdIsNull().andCreateUserIdEqualTo(cmd.getCreateUserId());
 			}
-			if(cmd.getCreateTimeBegin() != null){
+			if (cmd.getCreateTimeBegin() != null) {
 				c.andCreateTimeGreaterThanOrEqualTo(cmd.getCreateTimeBegin());
 			}
-			if(cmd.getCreateTimeEnd() != null){
+			if (cmd.getCreateTimeEnd() != null) {
 				c.andCreateTimeLessThanOrEqualTo(cmd.getCreateTimeEnd());
 			}
-			
+
 			if (cmd.getQueryScope() != null) {
 				if (new Integer(1).equals(cmd.getQueryScope())) {// 我的
-					if(cmd.getDoctor() != null){
+					if (cmd.getDoctor() != null) {
 						c.andDoctorIdIsNotNull().andDoctorIdEqualTo(cmd.getDoctor().getId());
 					}
 				} else if (new Integer(2).equals(cmd.getQueryScope())) {// 本诊所
-					if(cmd.getDoctor() != null){
+					if (cmd.getDoctor() != null) {
 						c.andHospitalIdEqualTo(cmd.getDoctor().getHospitalId());
 					}
-					if(cmd.getHospital() != null){
+					if (cmd.getHospital() != null) {
 						c.andHospitalIdEqualTo(cmd.getHospital().getId());
 					}
 				} else if (new Integer(3).equals(cmd.getQueryScope())) {// 全部
@@ -118,28 +118,28 @@ public class PhrBasicInfoService {
 			if (StringUtils.isNotBlank(cmd.getTel())) {
 				c.andTelLike("%" + cmd.getTel() + "%");
 			}
-			if(StringUtils.isNotBlank(cmd.getJddw())){
+			if (StringUtils.isNotBlank(cmd.getJddw())) {
 				c.andJddwLike("%" + cmd.getJddw() + "%");
 			}
-			if(cmd.getCreateUserId() != null && cmd.getCreateUserId() > 0){
+			if (cmd.getCreateUserId() != null && cmd.getCreateUserId() > 0) {
 				c.andDoctorIdIsNull().andCreateUserIdEqualTo(cmd.getCreateUserId());
 			}
-			if(cmd.getCreateTimeBegin() != null){
+			if (cmd.getCreateTimeBegin() != null) {
 				c.andCreateTimeGreaterThanOrEqualTo(cmd.getCreateTimeBegin());
 			}
-			if(cmd.getCreateTimeEnd() != null){
+			if (cmd.getCreateTimeEnd() != null) {
 				c.andCreateTimeLessThanOrEqualTo(cmd.getCreateTimeEnd());
 			}
 			if (cmd.getQueryScope() != null) {
 				if (new Integer(1).equals(cmd.getQueryScope())) {// 我的
-					if(cmd.getDoctor() != null){
+					if (cmd.getDoctor() != null) {
 						c.andDoctorIdIsNotNull().andDoctorIdEqualTo(cmd.getDoctor().getId());
 					}
 				} else if (new Integer(2).equals(cmd.getQueryScope())) {// 本诊所
-					if(cmd.getDoctor() != null){
+					if (cmd.getDoctor() != null) {
 						c.andHospitalIdEqualTo(cmd.getDoctor().getHospitalId());
 					}
-					if(cmd.getHospital() != null){
+					if (cmd.getHospital() != null) {
 						c.andHospitalIdEqualTo(cmd.getHospital().getId());
 					}
 				} else if (new Integer(3).equals(cmd.getQueryScope())) {// 全部
@@ -160,7 +160,7 @@ public class PhrBasicInfoService {
 		try {
 			BeanUtils.copyProperties(entity, cmd);
 			if (cmd.getId() != null && cmd.getId().intValue() > 0) {
-				//修改基本信息
+				// 修改基本信息
 				PhrBasicInfo old = phrBasicInfoMapper.selectByPrimaryKey(cmd.getId());
 				entity.setCreateTime(old.getCreateTime());
 				entity.setHasCover(old.getHasCover());
@@ -172,48 +172,50 @@ public class PhrBasicInfoService {
 				entity.setJddw(old.getJddw());
 				phrBasicInfoMapper.updateByPrimaryKey(entity);
 			} else {
-				//生成基本信息表
-				if(cmd.getDoctor() != null){
+				// 生成基本信息表
+				if (cmd.getDoctor() != null) {
 					Doctor doctor = cmd.getDoctor();
 					entity.setJdr(doctor.getRealname());
-					if(StringUtils.isBlank(doctor.getHospitalName()) || StringUtils.isBlank(doctor.getHospitalAddress())){
+					if (StringUtils.isBlank(doctor.getHospitalName())
+							|| StringUtils.isBlank(doctor.getHospitalAddress())) {
 						Hospital hospital = hopitalService.selectByPrimaryKey(doctor.getHospitalId());
-						if(hospital != null){
-							entity.setJddw(hospital.getAddress() + "，" + hospital.getWholeName());//建档单位
+						if (hospital != null) {
+							entity.setJddw(hospital.getAddress() + "，" + hospital.getWholeName());// 建档单位
 						}
-					}else{
-						entity.setJddw(doctor.getHospitalAddress() + "，" + doctor.getHospitalName());//建档单位
+					} else {
+						entity.setJddw(doctor.getHospitalAddress() + "，" + doctor.getHospitalName());// 建档单位
 					}
-				}else if(cmd.getAdmin() != null){
+				} else if (cmd.getAdmin() != null) {
 					entity.setJdr(cmd.getAdmin().getUsername());
 				}
 				phrBasicInfoMapper.insert(entity);
-				//生成封面信息表
+				// 生成封面信息表
 				PhrCover cover = new PhrCover();
-				cover.setName(entity.getName());//name
-				cover.setJdrq(new Date());//建档日期
-				cover.setLxdh(entity.getTel());//联系电话
-				cover.setCreateTime(new Date());//创建时间
-				cover.setBasicInfoId(entity.getId());//基本信息id
+				cover.setName(entity.getName());// name
+				cover.setJdrq(new Date());// 建档日期
+				cover.setLxdh(entity.getTel());// 联系电话
+				cover.setCreateTime(new Date());// 创建时间
+				cover.setBasicInfoId(entity.getId());// 基本信息id
 				cover.setPatientId(entity.getPatientId());
 				cover.setHospitalId(entity.getHospitalId());
 				cover.setDoctorId(entity.getDoctorId());
 				Doctor doctor = cmd.getDoctor();
-				if(doctor != null){
-					if(StringUtils.isBlank(doctor.getHospitalName()) || StringUtils.isBlank(doctor.getHospitalAddress())){
+				if (doctor != null) {
+					if (StringUtils.isBlank(doctor.getHospitalName())
+							|| StringUtils.isBlank(doctor.getHospitalAddress())) {
 						Hospital hospital = hopitalService.selectByPrimaryKey(doctor.getHospitalId());
-						if(hospital != null){
-							cover.setJddw(hospital.getAddress() + hospital.getWholeName());//建档单位
+						if (hospital != null) {
+							cover.setJddw(hospital.getAddress() + hospital.getWholeName());// 建档单位
 						}
-					}else{
-						cover.setJddw(doctor.getHospitalAddress() + doctor.getHospitalName());//建档单位
+					} else {
+						cover.setJddw(doctor.getHospitalAddress() + doctor.getHospitalName());// 建档单位
 					}
-					cover.setZrys(doctor.getRealname());//责任医生
+					cover.setZrys(doctor.getRealname());// 责任医生
 					cover.setJdr(doctor.getRealname());
 					cover.setCreateUserId(doctor.getId());
 				}
 				Admin admin = cmd.getAdmin();
-				if(admin != null){
+				if (admin != null) {
 					cover.setJdr(admin.getUsername());
 					cover.setCreateUserId(admin.getId());
 				}
@@ -255,5 +257,15 @@ public class PhrBasicInfoService {
 
 	public List<PhrBasicInfo> selectByPatientIds(List<Integer> patientIds) {
 		return phrBasicInfoMapper.selectByPatientIds(patientIds);
+	}
+
+	// 健康档案统计结果
+	public List<PhrBasicInfo> selectPhrCountList(PhrCountCmd cmd) {
+		return phrBasicInfoMapper.selectPhrCountList(cmd);
+	}
+
+	// 健康档案值
+	public Integer selectPhrCountValue(PhrCountCmd cmd) {
+		return phrBasicInfoMapper.selectPhrCountValue(cmd);
 	}
 }
